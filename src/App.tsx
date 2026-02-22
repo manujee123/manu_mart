93d7874a2b6fa3da8fdbb70d68687e30f195e8ee
kushaltm <kmmanoj689@gmail.com>
2026-02-22 11:22:37 +0530

    remove count

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import martlogo from './assets/manu.jpeg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
